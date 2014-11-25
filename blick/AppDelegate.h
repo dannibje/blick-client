921599e701fab8f14056b700b2f67f7fb247f98e
Daníel Björn Sigurbjörnsson <daniel.sigur@gmail.com>
2014-11-25 12:08:08 +0000

    Initial Commit

---
+++ blick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blick
+//
+//  Created by Daníel Björn Sigurbjörnsson on 11/25/14.
+//  Copyright (c) 2014 danni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blick
+//
+//  Created by Daníel Björn Sigurbjörnsson on 11/25/14.
+//  Copyright (c) 2014 danni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
